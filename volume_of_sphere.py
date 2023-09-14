5d7df6bc7dbf3e5679aa7d343706c73d2b4cef4a
Rachel Vickery <rachelvickery@wireless-10-155-73-111.public.utexas.edu>
2023-09-14 16:53:52 -0500

    added a new files

---
+++ volume_of_sphere.py
@@ -0,0 +1,13 @@
+def calculate_volume_of_sphere(radius):
+	pi = 3.14
+	area = 4/3 * pi * radius * radius * radius
+	return area
+
+radius1 = 30
+area1 = compute_area_of_circle(radius1)
+print(f"The volume of a sphere {radius1} is: {area1}")
+
+radius2 = 40
+area2 = compute_area_of_circle(radius2)
+print(f"The volume of a sphere {radius2} is: {area2}")
+
